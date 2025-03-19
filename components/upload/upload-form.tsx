96e50cf8e1a16419a4ddcd43eeb6ee95547a4720
romatorres <romatorres12@gmail.com>
2025-03-19 17:24:39 -0300

    up form

--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,10 +1,36 @@
 "use client";
 
 import UploadFormInput from "./upload-form-input";
+import { z } from "zod";
+
+const schema = z.object({
+  file: z
+    .instanceof(File, { message: "Arquivo invalido" })
+    .refine((file) => file.size <= 20 * 1024 * 1024, "Arquivo maior que 20mb")
+    .refine(
+      (file) => file.type.startsWith("application/pdf"),
+      "O arquivo deve ser um PDF"
+    ),
+});
 
 export default function UploadForm() {
-  const handleSubmit = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
     console.log("Submetido");
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("file") as File;
+
+    const validatedFields = schema.safeParse({ file });
+
+    console.log(validatedFields);
+
+    if (!validatedFields.success) {
+      console.log(
+        validatedFields.error.flatten().fieldErrors.file?.[0] ??
+          "Arquivo invalido"
+      );
+      return;
+    }
   };
 
   return (
